0f2e4c4a88b39b69b0ce98da7600e0bdc4adb22b
Camilo Jose Cruz Rivera <cruz.camilo@correounivalle.edu.co>
2019-02-22 07:06:58 -0500

    crear nuevo heroe

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,12 +10,27 @@ import { Hero } from '../hero'
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
-  
+  visibility = false;
+  new_name = "";
+
+
   constructor(private heroService: HeroService) { }
 
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
+  }
+
+  showInput(): void {
+    this.visibility = !this.visibility; 
+  }
+
+  addHero(): void {
+    let nuevo_heroe: Hero;
+    var ultimo = this.heroes[this.heroes.length - 1].id + 1;
+    nuevo_heroe = {id: ultimo, name: this.new_name};
+    this.heroes.push(nuevo_heroe);
+    this.visibility = !this.visibility; 
   }
 
   ngOnInit() {
